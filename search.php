5db5cc0e185008fa9cb050dd03b24db7179abf9b
chy893409115 <34466589+chy893409115@users.noreply.github.com>
2017-12-22 20:41:18 +0800

    Add files via upload

---
+++ search.php
@@ -0,0 +1,11 @@
+<?php
+$kw=$_REQUEST["term"];
+$conn = mysqli_connect('127.0.0.1', 'root', '', 'mx', 3306);
+mysqli_query($conn, "SET NAMES UTF8");
+$sql="SELECT title FROM mx_laptop WHERE title LIKE '%".$kw."%' ORDER BY sold_count DESC LIMIT 10";
+$result = mysqli_query($conn, $sql);
+$arr=[];
+foreach($result as $row){
+  $arr[]=$row["title"];
+}
+echo json_encode($arr);
